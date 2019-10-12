e3e5ba94c11ac75d7ce3cb0ed570b2ea182dde0f
Rama chandra <ramachandrared@hcl.com>
2019-10-12 14:06:27 +0530

    Initial Commit of the Spring Boot base Repo

---
+++ src/test/java/com/hcl/openbank/technightsapp/TechnightsappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hcl.openbank.technightsapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TechnightsappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
